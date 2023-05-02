603031f8e86525d60ca736045c834c5a2202bd2c
MikeOxMedium <111542535+MikeOxMedium@users.noreply.github.com>
2023-05-02 13:29:10 +1200

    Add files via upload

--- 02_RPS/02_user_choice_v3.py
+++ 02_RPS/02_user_choice_v3.py
@@ -1,43 +1,28 @@
-
 # Functions go here
 def choice_checker(question, valid_list, error):
-
-    error = "Please choose from rock / paper/ scissors (or xxx to quit)"
-
-    valid = False
-    while not valid:
+    while True:
 
         # Ask user for choice (and put in lowercase)
         response = input(question).lower()
 
         for item in valid_list:
             if response == item[0] or response == item:
-                return response
+                return item
 
         print(error)
         print()
 
-        if response == "r" or response == "rock":
-            return response
-        elif response == "p" or response == "paper":
-            return response
-        elif response == "s" or response == "scissors":
-            return response
-
-        elif response == "xxx":
-            return response
-        else:
-            print(error)
 
 # Main routine goes here
 
+rps_list = ["rock", "paper", "scissors", "xxx"]
 
 # Loop for testing purposes
 user_choice = ""
 while user_choice != "xxx":
-
     # Ask user for choice and check its valid
-    user_choice = choice_checker("choice rock / paper/ scissors (r/p/s): , Please choose from rock / paper/ scissors (or xxx to quit)")
+    user_choice = choice_checker("choice rock / paper/ scissors (r/p/s):", rps_list,
+                                 "Please choose from rock / paper/ scissors (or xxx to quit)")
 
     # print out choice for comparison purposes
-    print("You chose{}".format(user_choice))
+    print(f"You chose {user_choice }")
